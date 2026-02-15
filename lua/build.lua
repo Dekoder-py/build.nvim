9e1581dcb67d9542b88d271d035f2d873a194afe
Dekoder-py <dekod3r.py@gmail.com>
2026-02-16 11:17:41 +1300

    add support for just.systems

--- lua/build.lua
+++ lua/build.lua
@@ -57,6 +57,7 @@ local function find_build_system(root)
 		["meson.build"] = "meson",
 		["package.json"] = "npm",
 		["setup.py"] = "setuptools",
+    ["Justfile"] = "just",
 	}
 	local file = vim.fs.find(vim.tbl_keys(compilers), { path = path })[1]
 	return compilers[vim.fs.basename(file)]
